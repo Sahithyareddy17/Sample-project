03d9f18fa24cd67617254af88398467f238a3c82
Sahithyareddy17 <kondapallysahithyareddy@gmail.com>
2025-08-17 11:33:26 +0530

    Update AppTest.java

--- my-app/src/test/java/com/example/AppTest.java
+++ my-app/src/test/java/com/example/AppTest.java
@@ -2,7 +2,8 @@ package com.example;
  
 import org.junit.jupiter.api.Test;
  
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static 
+org.junit.jupiter.api.Assertions.assertEquals;
  
 class AppTest {
     @Test
