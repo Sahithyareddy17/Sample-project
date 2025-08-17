7530131eb837a19459d65679a9ac4d6c03f481d2
Sahithyareddy17 <kondapallysahithyareddy@gmail.com>
2025-08-17 08:41:09 +0530

    Update AppTest.java

--- my-app/src/test/java/com/example/AppTest.java
+++ my-app/src/test/java/com/example/AppTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  
 class AppTest {
     @Test
-    void greetsByName() {
-        assertEquals("Hello, Sahithya!", App.greet("Sahithya"));
+    void greetsWorld() {
+        assertEquals("Hello, World!", App.greet("World"));
     }
 }
