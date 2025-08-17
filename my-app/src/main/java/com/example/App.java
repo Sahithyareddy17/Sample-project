4f52d8cbd922e412834596a8514b2b2b83e16f0e
Sahithyareddy17 <kondapallysahithyareddy@gmail.com>
2025-08-17 11:54:11 +0530

    Update App.java

--- my-app/src/main/java/com/example/App.java
+++ my-app/src/main/java/com/example/App.java
@@ -2,10 +2,9 @@ package com.example;
  
 public class App {
   public static String greet(String name) {
-    return "Hello," + name + "!";
-   
+    return "Hello," + name + "!"; 
   }
   public static void main(String[] args) {
       System.out.println(greet("World"));
-    }
+  }
 }
